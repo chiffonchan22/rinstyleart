7c917c9fab92006d3235e46b589acb5319af1a78
chiffonchan22 <chiffonchan.22@gmail.com>
2025-10-25 19:56:16 +0900

    update20251025

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
   const nav = document.querySelector('nav');
-  if (window.scrollY > 50) { // 300pxスクロールしたら切り替え
+  if (window.scrollY > 40) { // 300pxスクロールしたら切り替え
     nav.classList.add('scrolled');
   } else {
     nav.classList.remove('scrolled');
